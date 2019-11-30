0e0f451609e967326772bd72351e3252a3e13473
Mars Zheng <zheng.xiangfei@gmail.com>
2019-11-30 16:45:11 +0800

    domain event

---
+++ src/app/common/domain-event.ts
@@ -0,0 +1,32 @@
+
+export type EventAction = (eventArgs: any) => void;
+
+
+export default class DomainEvent {
+
+  private static readonly instance: DomainEvent = new DomainEvent();
+
+  private actionList: EventAction[] = [];
+
+  constructor() {
+    return DomainEvent.instance;
+  }
+
+  public static getInstance() {
+    return this.instance;
+  }
+
+  public raise(eventArgs: any) {
+    this.actionList.forEach(
+      action => {
+        action(eventArgs);
+      }
+    );
+  }
+
+  public Register(action: EventAction) {
+    this.actionList.push(action);
+  }
+
+
+}
